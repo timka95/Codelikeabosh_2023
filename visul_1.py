458d324e3abd509a24e5d45d64c974c103d14802
Timka3435 <69326246+timka95@users.noreply.github.com>
2023-09-29 19:38:24 +0200

    Add files via upload

---
+++ visul_1.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load your data from a CSV file (replace 'your_data.csv' with your actual data file)
+data = pd.read_csv('/Users/timeanemet/Desktop/Hachatlon/DevelopmentData.xlsx - Sheet1.csv')
+
+# Extract X and Y data
+x_data = data["FourthObjectDistance_X"]
+y_data = data["FourthObjectDistance_Y"]
+
+# Create a color map based on time
+time_values = range(1, len(data) + 1)
+normalize = plt.Normalize(min(time_values), max(time_values))
+cmap = plt.get_cmap('viridis')
+
+# Create a figure and axis
+fig, ax = plt.subplots()
+
+# Plot the data points with colors representing time
+sc = ax.scatter(x_data, y_data, c=time_values, cmap=cmap, marker='o', label='Data Points')
+
+# Add a color bar to indicate time
+cbar = plt.colorbar(sc, ax=ax)
+cbar.set_label('Time')
+
+ax.scatter(0, 0, label='Vehicle', marker='s', color='red')
+
+# Set labels and title
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_title('Fourth X and Y Over Time')
+
+# Show the plot
+plt.show()
---
+++ visul_XY.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+# Load your data from a CSV file (replace 'your_data.csv' with your actual data file)
+data = pd.read_csv('/Users/timeanemet/Desktop/Hachatlon/DevelopmentData.xlsx - Sheet1.csv')
+
+# Extract YawRate and Vehicle Speed data
+yaw_rate = data["YawRate"]
+vehicle_speed = data["VehicleSpeed"]
+time_values = data["Timestamp"]  # Use the "TimeStamp" column as time values
+
+# Initialize the vehicle position at (0, 0)
+x_position = [0]
+y_position = [0]
+
+# Calculate the new positions based on YawRate and Vehicle Speed
+for i in range(1, len(data)):
+    delta_x = vehicle_speed[i] * yaw_rate[i]
+    delta_y = vehicle_speed[i]
+
+    x_position.append(x_position[-1] + delta_x)
+    y_position.append(y_position[-1] + delta_y)
+
+# Create a colormap based on time
+cmap = ListedColormap(plt.cm.viridis(time_values / max(time_values)))
+
+# Create a figure and axis
+fig, ax = plt.subplots()
+
+# Plot the vehicle's position over time with colormap
+sc = ax.scatter(x_position, y_position, c=time_values, cmap=cmap, marker='o', label='Vehicle Position')
+
+# Add a color bar to indicate time
+cbar = plt.colorbar(sc, ax=ax, label='Time')
+
+# Set labels and title
+ax.set_xlabel('X Position')
+ax.set_ylabel('Y Position')
+ax.set_title('Vehicle Position Over Time with Colormap')
+
+# Show the legend
+ax.legend()
+
+# Show the plot
+plt.show()
